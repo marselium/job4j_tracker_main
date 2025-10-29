e868273be4b35b2b670cccf522869cfe50bea6b0
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-10-29 22:29:13 +0300

    1. Sql схема для проекта Tracker. [#276987 #464081]

---
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,68 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+
+import java.util.Objects;
+
+public class Item {
+    private LocalDateTime created = LocalDateTime.now();
+    private int id;
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,65 @@
+package ru.job4j.tracker;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class Tracker {
+    private final List<Item> items = new ArrayList<>();
+
+    private int ids = 1;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items.add(item);
+        return item;
+    }
+
+    public List<Item> findAll() {
+        return List.copyOf(items);
+    }
+
+    public List<Item> findByName(String key) {
+        List<Item> result = new ArrayList<>();
+        for (Item it : items) {
+            if (key.equals(it.getName())) {
+                result.add(it);
+            }
+        }
+        return result;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items.get(index) : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int i = indexOf(id);
+        boolean rsl = i != -1;
+        if (rsl) {
+            item.setId(id);
+            items.set(i, item);
+        }
+        return rsl;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId() == id) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public void delete(int id) {
+        int i = indexOf(id);
+        boolean rsl = i != -1;
+        if (rsl) {
+           items.remove(i);
+        }
+    }
+}
